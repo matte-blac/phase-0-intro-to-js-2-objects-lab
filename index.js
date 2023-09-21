2f3b1cf4e56b3b70ed0ee08028d7ac7b32c1e3c5
Matthew Bomett <bomettmat2@gmail.com>
2023-09-21 19:06:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+
+
+const employee = {}
+
+//non-destructive update
+function updateEmployeeWithKeyAndValue(employees, key, value){
+    const employee = { ...employees}
+
+    employee[key] = value
+
+    return employee
+}
+const updated = updateEmployeeWithKeyAndValue(employee, 'name', 'Sam')
+console.log(updated)
+
+//destructive Update
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee
+}
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'name', 'Sam')
+assert.deepEqual(employee, {name: 'Sam', streetAddress: '11 Broadway'}) 
+
+//non-destructive delete
+function deleteFromEmployeeByKey(employees, key){
+    const employee = { ...employees}
+
+    delete employee[key]
+
+    return employee
+}
+
+//destructive delete
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+
+    return employee
+}
